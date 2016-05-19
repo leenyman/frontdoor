12ebe80202909e0681e5f3f4d724795e19d1f06c
lee nyman <lnyman@tapqa.com>
2016-05-19 18:04:12 -0500

    adding upload logic

---
+++ apiupload.py
@@ -0,0 +1,8 @@
+import requests, json
+
+data = json.load(open("data.json","rb"))
+f = {"file": open("capture.jpg", "rb")}
+url = "https://slack.com/api/files.upload"
+payload = {"token": data['token'],"filename" : "front door capture", "channels": "#tap_u_tigers", "fileype": "jpg", "pretty" : 1}
+r = requests.post(url,params=payload,files = f)
+print(r.text)
