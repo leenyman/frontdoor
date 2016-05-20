921ebac2d8c96bff98dedb407fb442e5b764ca56
lee nyman <lnyman@tapqa.com>
2016-05-20 13:35:03 -0500

    added a ping to the upload portion

--- apiupload.py
+++ apiupload.py
@@ -4,4 +4,7 @@ data = json.load(open("data.json","rb"))
 f = {"file": open("capture.jpg", "rb")}
 url = "https://slack.com/api/files.upload"
 payload = {"token": data['token'], "filename" : "front door capture", "channels": "#tap_u_tigers", "fileype": "jpg", "pretty" : 1}
+message = {"token": data['token'], "text": "@channel someone is at the front door", "channel": "#tap_u_tigers", "parse": "full", "as_user": "true", "pretty" : 1}
+resp = requests.post("https://slack.com/api/chat.postMessage",params=message)
 r = requests.post(url,params=payload,files = f)
+print(resp.text)
