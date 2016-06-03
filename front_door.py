09adadde2b9f54ad1f9081b88df39498b976890d
leenyman <lnyman@tapqa.com>
2016-06-03 16:14:58 -0500

    moved channel to json file and added time of day logic to checks

--- front_door.py
+++ front_door.py
@@ -4,18 +4,36 @@ import RPi.GPIO as GPIO
 import time
 import picamera
 import slack_api
+from sys import argv
 
+#default start and end times
+start = 8
+end = 18
+
+#checking for args and setting the vaules correctly with a check for military time
+if len(argv) >=3:
+	start = int(argv[1])
+	end = int(argv[2])
+	if end < 12:
+		end +=12
+else:
+	print("Invalid args, falling back on default 8am to 6pm behaviour.")
+	
+#sensor setup
 sensor = 4
 camera = picamera.PiCamera()
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
-previous_state = False
-current_state = False
-
+#main logic
 while True:
-	current_state = GPIO.input(sensor)
+	#set state to false and only update between the start and end times of day
+	current_state = False
+	if start <= time.localtime().tm_hour < end:
+		current_state = GPIO.input(sensor)
+
+	#check status of user and upload photos if target user is away
 	if current_state and slack_api.check() =="away":
 		camera.capture('capture.jpg')
 		print("captured")
@@ -24,4 +42,4 @@ while True:
 		time.sleep(60)
 		print("slept")
 	else:
-                time.sleep(.5)
+                time.sleep(1)
